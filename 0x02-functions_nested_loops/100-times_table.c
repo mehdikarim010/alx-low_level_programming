7bb3cb2597e29944611406647264e84949a52225
mehdikarim010 <karimmehdi1994@gmail.com>
2023-06-20 03:57:32 -0700

    100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,8 +8,6 @@
 void print_times_table(int n)
 {
 int i, j, k;
-
-
 if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
@@ -21,7 +19,7 @@ if (j == 0)
 {
 _putchar(k + '0');
 }
-else if (k < 10 && j != 0)
+else if (k < 10)
 {
 _putchar(',');
 _putchar(' ');
@@ -29,21 +27,21 @@ _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
 }
-else if (k >= 10 && k < 100)
+else if (k < 100)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
 }
-else if (k >= 100)
+else
 {
 _putchar(',');
 _putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
+_putchar(k / 100 + '0');
+_putchar((k / 10) % 10 + '0');
+_putchar(k % 10 + '0');
 }
 }
 _putchar('\n');
